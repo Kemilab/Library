1553fd38d117e119f83d109fc4a2cf9347b665fe
Luka Salaj <lukasalaj9@gmail.com>
2022-06-01 09:36:05 +0200

    Dodana prijava admina

--- Library.cpp
+++ Library.cpp
@@ -1,22 +1,58 @@
 
 #include <iostream>
 #include<stdio.h>
+#include <string.h>
 #include<algorithm>
 #include<math.h>
 #include<fstream>
 
 using namespace std;
+void admin()
+{
+    int izbor = 0;
+    while (izbor != 9)
+    {
+        cout << "\n\n Admin Login is Successful !! Press any key to continue..";
+        cout << "\n1. Add Book";
+        cout << "\n2. Update Book";
+        cout << "\n3. Delete Book";
+        cout << "\n4. List of All Books";
+        cout << "\n9. Exit";
+        cout << "\n\n Enter your choice : ";
+        cin >> izbor;
+        switch (izbor)
+        {
+        case 1:
+            //addBook();
+            break;
+        case 2:
+            //updateBook();
+            break;
+        case 3:
+            //deleteBook();
+            break;
+        case 4:
+            // listOfAllBooks();
+            break;
+        default:
+            cout << "\n\n Invalid Choice. Press any key to continue..";
+            getchar();
+        }
+        system("cls");
+    }
+}
 
 int main()
 
 {
+    string knjiznicar;
+    string Password;
     int izbor;
     int bookN;
     string kime;
-    string kime8;
     string knjiga;
     char next;
-    while (1)
+    while (izbor != 3)
     {
         system("cls");
         cout << "---------------------------------------------------------------------------" << endl;
@@ -52,8 +88,57 @@ int main()
         }
         if (izbor == 2)
         {
-            break;
+            int izborK;
+            cout << "1 Registracija" << endl;
+            cout << "2 Prijava" << endl;
+            cin >> izborK;
+            //Options for libraryan
+            //Sign up
+            if(izborK==1)
+            {
+                string KNJIZNICAR;
+                string PASSWORD;
+
+                cout<<"\nUNESITE SVOJE KORISNICKO IME:";
+                cin>>KNJIZNICAR;
+                cout<<"\nUNESITE SVOJU SIFRU:";
+                cin>>PASSWORD;
+                system("cls");
+                cout << "1 Registracija" << endl;
+                cout << "2 Prijava" << endl;
+            }
+                izborK = 0;
+                cin >> izborK;
+
+            //Sign in
+            if (izborK == 2)
+            {
+                string KNJIZNICAR;
+                string PASSWORD;
+                string knjiznicar;
+                string Password;
+
+
+                system("cls");
+                cout << "\n ---------- Prijava knjiznicara -------";
+                cout << "\n\n Upisite korisnicko ime: ";
+                cin >> knjiznicar;
+                cout << "\n Upisite lozinku:";
+                cin >> Password;
+                if(Password == PASSWORD)
+                {
+                    admin();
+                }
+                /*else
+                {
+                    cout << "Krivi korisnicki podatci!" << endl;
+                    break;
+                }*/
+
+                system("cls");
+            }
         }
+
         if (izbor == 3)
         {
             cout << "Pozdrav!" << endl;
@@ -64,7 +149,9 @@ int main()
             cout << "Krivi unos!" << endl;
             break;
         }
-
+        return 0;
     }
-    return 0;
+
 }
+
+
