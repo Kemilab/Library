cdda27f2032c41deac26a0d0312af0d7986a560f
Luka Salaj <lukasalaj9@gmail.com>
2022-06-01 08:32:15 +0200

    Dodana opcija 3

--- Library.cpp
+++ Library.cpp
@@ -10,63 +10,61 @@ using namespace std;
 int main()
 
 {
-	int izbor;
-	int bookN;
-	string kime;
-	string kime8;
-	string knjiga;
-	char next;
-	while (1)
-	{
-		system("cls");
-		cout << "---------------------------------------------------------------------------" << endl;
- 		cout << "DOBRODOSLI U KNJIZNICU\n" << endl;
- 		cout << "---------------------------------------------------------------------------" << endl;
-		cout << "1 Prijava ucenika" << endl;
-		cout << "2 Prijava profesora" << endl;
-		cout << "3 Prijava knjiznicara\n" << endl;
-		cin >> izbor;
-		//different choices output
-		if (izbor == 1)
-		{
-			fstream korisnik("korisnik.bin", ios::binary | ios::out);
-			cout << "Odabrali ste prijavu ucenika!" << endl;
-			cout << "Upisite korisnicko ime: ";
-			cin >> kime;
-			korisnik.write((char*)&kime, sizeof(kime));
-			korisnik.close();
-			korisnik.open("korisnik.bin", ios::binary | ios::in);
-			korisnik.read((char*)&kime, sizeof(kime));
-			korisnik >> kime;
-			korisnik.close();
-			cout << "Pozdrav!  " << kime << endl;
-			system("Pause");
-			cout << "Ucitavam dostupni popis knjiga" << endl;
-			cout << "\nMama je kriva za sve\n Pale sam na svijetu\n Kako si\n Putar i parizer\n Crvena voda\n " << endl;
-			cout << "Upisite naziv knjige koje posudujete: ";
-			cin >> knjiga;
-			korisnik.open("korisnik.bin", ios::binary | ios::app);
-			korisnik.write((char*)&knjiga, sizeof(knjiga));
-			korisnik.close();
-			system("pause");
+    int izbor;
+    int bookN;
+    string kime;
+    string kime8;
+    string knjiga;
+    char next;
+    while (1)
+    {
+        system("cls");
+        cout << "---------------------------------------------------------------------------" << endl;
+        cout << "                       DOBRODOSLI U KNJIZNICU\n" << endl;
+        cout << "---------------------------------------------------------------------------" << endl;
+        cout << "                       1 Prijava ucenika" << endl;
+        cout << "                       2 Prijava knjiznicara" << endl;
+        cout << "                       3 Izlaz" << endl;
+        cin >> izbor;
+        //different choices output
+        if (izbor == 1)
+        {
+            fstream korisnik("korisnik.bin", ios::binary | ios::out);
+            cout << "Odabrali ste prijavu ucenika!" << endl;
+            cout << "Upisite korisnicko ime: ";
+            cin >> kime;
+            korisnik.write((char*)&kime, sizeof(kime));
+            korisnik.close();
+            korisnik.open("korisnik.bin", ios::binary | ios::in);
+            korisnik.read((char*)&kime, sizeof(kime));
+            korisnik >> kime;
+            korisnik.close();
+            cout << "Pozdrav!  " << kime << endl;
+            system("Pause");
+            cout << "Ucitavam dostupni popis knjiga" << endl;
+            cout << "\nMama je kriva za sve\n Pale sam na svijetu\n Kako si\n Putar i parizer\n Crvena voda\n " << endl;
+            cout << "Upisite naziv knjige koje posudujete: ";
+            cin >> knjiga;
+            korisnik.open("korisnik.bin", ios::binary | ios::app);
+            korisnik.write((char*)&knjiga, sizeof(knjiga));
+            korisnik.close();
+            system("pause");
+        }
+        if (izbor == 2)
+        {
+            break;
+        }
+        if (izbor == 3)
+        {
+            cout << "Pozdrav!" << endl;
+            break;
+        }
+        else
+        {
+            cout << "Krivi unos!" << endl;
+            break;
+        }
 
-
-
-
-
-
-
-		}
-		else if (izbor == 2)
-		{
-			break;
-		}
-		else
-		{
-			cout << "Krivi unos!" << endl;
-			break;
-		}
-
-	}
-	return 0;
+    }
+    return 0;
 }
