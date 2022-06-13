00d2cab976f842cce516b595e76c955081d20ae3
Luka Salaj <lukasalaj9@gmail.com>
2022-06-13 18:14:34 +0200

    Optimizacija

--- Library.cpp
+++ Library.cpp
@@ -36,7 +36,7 @@ void blokiran_racun()//dodati oporavak baze tako da stvori datoteku za knjiznica
    printf("\n");
 
 }
-void admin()
+void knjiz()
 {
     int izbor = 0;
     while (izbor != 9)
@@ -72,6 +72,9 @@ void admin()
 }
 
 int main() {
+    fstream korisnik("posudba.bin", ios::binary | ios::out);
+    fstream datoteka("knjige.bin", ios::binary | ios::out);
+
     string p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50;
     p1 = "1000 katastrofa";
     p2 = "99 vrucih tragova";
@@ -123,7 +126,6 @@ int main() {
     p48 = "Velike bitke II.svjetskog rata";
     p49 = "Zagonetni djecak";
     p50 = "Zivotinjska farma";
-    fstream datoteka("knjige.bin", ios::binary | ios::out);
     datoteka.read((char*)&p1, sizeof(p1));
     datoteka.read((char*)&p2, sizeof(p2));
     datoteka.read((char*)&p3, sizeof(p3));
@@ -423,6 +425,7 @@ int main() {
                 korisnik.read((char*)&knjiga, sizeof(knjiga));
                 cout << "Trenutno je posudjena: " << knjiga << endl;
                 cout << "Zelite vratiti knjigu? (Y/N)" << endl;
+                korisnik.close();
                 while (1)
                 {
                     cin >> izbor;
@@ -431,7 +434,7 @@ int main() {
                     izbor = tolower(izbor);
                     if (izbor == 'y')
                     {
-                        remove("posudba.bin");
+                        korisnik.open("posudba.bin", ios::binary | ios::trunc);
                         cout << "Knjiga je vracena :)" << endl;
                         sleep_for(seconds(2));
                         goto meni;
@@ -638,7 +641,7 @@ int main() {
                 datoteka.open("knjiznicar_prijava.bin", ios::binary | ios::in);
                 datoteka.read((char*)&PASSWORD, sizeof(PASSWORD));
                 if (PASSWORD == Password) {
-                    admin();
+                    knjiz();
                     datoteka.close();
                 }
                 else if (PASSWORD != Password) {
